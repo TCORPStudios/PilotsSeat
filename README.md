eb425eac4d70039c669f7e6b2532a848d6363bb6
Trinity Martinez <60677577+TCORPStudios@users.noreply.github.com>
2022-11-10 22:59:18 +0000

    Updated to v0.1a

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ I was going to make this on a TI-83+ calculator, but Python proved to be better 
 I'm trying to make the code easily modifiable and understandable, but it's probably gonna be a rat's nest.
 
 My journey of coding this will definitely be a learning experience, and I hope that it will be a good game in the end!
+
+---
+# This project has been revived!
+I last worked on this in March of 2021. It's been forever since I've last seen this code, and I am so glad that I bothered to comment this code. My ability to program Python has gotten rusty in recent times, so this code I wrote a year and a half ago might help me get back on my feet.
+It is now November 2022, and I wanna finish this code.
--- main.py
+++ main.py
@@ -8,26 +8,28 @@ import replit
 fakeCPU = 1  #Fake CPU delay for time.sleep function.
 statChecks = 0  #How many times the user has ran program 101: STATUS.
 programsRan = 0  #How many programs the user has ran.
-fuelMax = 1000 #Ship fuel capacity
-fuelLvl = fuelMax #Ship fuel is equal to max at init.
-fuelToLand = 100 #Fuel required to land or ascend.
-cellMax = 5 #Fuel cell kick random max.
-cellMin = 0 #Fuel cell kick random minimum.
+fuelMax = 1000  #Ship fuel capacity
+fuelLvl = fuelMax  #Ship fuel is equal to max at init.
+fuelToLand = 100  #Fuel required to land or ascend.
+cellMax = 5  #Fuel cell kick random max.
+cellMin = 0  #Fuel cell kick random minimum.
+commandCost = 5  #Fuel required to execute a command
 
 #Initialization and titles
 replit.clear()
-print(Fore.BLUE + "Pilot's Seat")
-time.sleep(fakeCPU*2)
-print(Fore.WHITE + "Code and design (C) 2021 Trinity K. Martinez")
-time.sleep(fakeCPU*2)
-print(Fore.WHITE + "(C)2021 TCORP Studios")
-time.sleep(fakeCPU*2)
+print(Fore.BLUE + "Pilot's Seat" + Fore.GREEN + "v0.1a")
+time.sleep(fakeCPU * 2)
+print(Fore.WHITE + "Code and design (C) 2021-2022 Trinity K. Martinez")
+time.sleep(fakeCPU * 2)
+print(Fore.WHITE + "(C)2021 TCORP Studios\n(C)2022 AUVIMA Software")
+time.sleep(fakeCPU * 2)
 replit.clear()
 
 #codeList contains all code IDs in order.
 codeList = [
     "101: STATUS", "102: RADAR ", "201: LAND  ", "211: ASCEND", "301: OPNDOR",
-    "311: CLSDOR", "401: TGTMEN", "402: GTOTGT", "501: EMRGNC", "502: ABORT ", "601: FULCEL"
+    "311: CLSDOR", "401: TGTMEN", "402: GTOTGT", "501: EMRGNC", "502: ABORT ",
+    "601: FULCEL"
 ]
 #codeNumbers identifies all codes.
 codeNumbers = [
@@ -39,7 +41,10 @@ codeNumbers = [
 def codeInput():
     varCode = input(Fore.WHITE + "Input Code:")
     time.sleep(fakeCPU)
-    codeInterpreter(varCode)
+    if fuelLvl <= 0:  #When fuelLvl reaches 0 or lower, end the game.
+      endGame()
+    else:
+      codeInterpreter(varCode)
 
 
 #codeInterpreter is run to identify the user input, and determine user errors.
@@ -65,8 +70,16 @@ def codeInterpreter(code):
 def progRun(programVar):  #progRun is ran to interpret input and run programs.
     global programsRan
     global codeNumbers
-    codesMax = ((len(codeNumbers))) #Makes a variable that measures how many codes there are.
-    for i in range(0, codesMax, 1):  #Makes a loop from 0 to the length of codeNumbers via the codesMax variable.
+    global fuelLvl
+    global commandCost
+  
+    fuelLvl = fuelLvl - commandCost  #Every command costs a certain ammount of fuel
+  
+    codesMax = ((len(codeNumbers))
+                )  #Makes a variable that measures how many codes there are.
+    for i in range(
+            0, codesMax, 1
+    ):  #Makes a loop from 0 to the length of codeNumbers via the codesMax variable.
         if programVar == i:
             print(Fore.WHITE + "Running program #" +
                   str(codeNumbers[programVar]))
@@ -83,32 +96,57 @@ def progRun(programVar):  #progRun is ran to interpret input and run programs.
 #Program functions below. If adding another program, then an entry must be added to here in addition codeList and codeNumbers.
 
 
-def prog0(): #Program 101, status checking.
+def prog0():  #Program 101, status checking.
     global statChecks
+    global fuelLvl
     statChecks += 1
     time.sleep(fakeCPU)
     print(Fore.BLUE + "STATUS CHECKS:" + Fore.WHITE + str(statChecks))
     time.sleep(fakeCPU)
     print(Fore.GREEN + "PROGRAMS RAN :" + Fore.WHITE + str(programsRan))
-
+    time.sleep(fakeCPU)
+    print(Fore.YELLOW + "FUEL LEVEL   :" + Fore.WHITE + str(fuelLvl))
+    time.sleep(fakeCPU)
     codeInput()
-def prog10(): #Program 601, Fuel Cell kick.
+
+
+def prog10():  #Program 601, Fuel Cell kick.
     global fuelMax
     global fuelLvl
     global cellMax
     global cellMin
     time.sleep(fakeCPU)
     print(Fore.YELLOW + "Fuel Cell Kick in progress...")
-    time.sleep(fakeCPU*2)
-    fuelGain = randint(cellMin, cellMax) #Makes new variable to store how much the random chance yeilded.
-    fuelLvl += fuelGain #Adds fuelGain variable to fuelLvl variable.
-    print(Fore.GREEN + "Fuel gained from Fuel Cell:" + Fore.WHITE + str(fuelGain))
+    time.sleep(fakeCPU * 2)
+    fuelGain = randint(
+        cellMin, cellMax
+    )  #Makes new variable to store how much the random chance yeilded.
+    fuelLvl += fuelGain  #Adds fuelGain variable to fuelLvl variable.
+    print(Fore.GREEN + "Fuel gained from Fuel Cell:" + Fore.WHITE +
+          str(fuelGain))
     fuelCheck()
+
+
+def fuelCheck(
+):  #Checks if fuel goes over maximum capacity, and adjusts accordingly.
+    global fuelMax
+    global fuelLvl
+    if fuelLvl > fuelMax:
+        fuelLvl = fuelMax
+    codeInput()
+
+def endGame(
+):  #Ending the game.
+  print(Fore.RED + "Code Input Error #004 (Insufficient Fuel to Execute)\nCannot Retry. Powering Down" + Fore.WHITE)
+  time.sleep(fakeCPU*2)
+  replit.clear()
+  time.sleep(fakeCPU*3)
+  print("Game Over")
+  time.sleep(fakeCPU)
+  print(Fore.BLUE + "STATUS CHECKS:" + Fore.WHITE + str(statChecks))
+  time.sleep(fakeCPU)
+  print(Fore.GREEN + "PROGRAMS RAN :" + Fore.WHITE + str(programsRan))
+  time.sleep(fakeCPU)
   
-def fuelCheck(): #Checks if fuel goes over maximum capacity, and adjusts accordingly.
-  global fuelMax
-  global fuelLvl
-  if fuelLvl > fuelMax:
-    fuelLvl = fuelMax
+if fuelLvl > 0:
   codeInput()
-codeInput()
